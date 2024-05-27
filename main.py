750c68dd3d157cb029bdf45117223d7b7aba41bc
Koray7 <softwarekoray@gmail.com>
2024-05-27 16:58:01 -0500

    More practice

--- main.py
+++ main.py
@@ -164,3 +164,84 @@
 #             print("You played very well but wrong answer, game Over!")
 # else:
 #     print("Game Over!")
+
+
+# ***************Treasure Map***************
+
+# line1 = [" "," "," "]
+# line2 = [" "," "," "]
+# line3 = [" "," "," "]
+
+# map = [line1, line2, line3]
+
+# print("Hiding your treasure X marks the spot.")
+# print(f"{line1}\n{line2}\n{line3}")
+# position = input("Where do you want to put your treasure?:\n")
+
+# letter = position[0].lower()
+# index_num = int(position[1]) - 1
+
+# abc = ["a","b","c"]
+# letter_index = abc.index(letter)
+
+# map[index_num][letter_index] = "X"
+
+# print(f"{line1}\n{line2}\n{line3}")
+
+# ************Rock, Paper, Scissors*****************
+
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game_images = [rock, paper, scissors]
+
+print("Welcome to Rock, Paper, Scissors game!\n")
+
+player_choice = int(input("Please type 0 'Rock', 1 for 'Paper' or 2 for 'Scissors' to see if you win or lose the game, let's get started!\n"))
+
+if player_choice >= 3 or player_choice < 0: 
+  print("You typed an invalid number, you lose!") 
+else: 
+    print(game_images[player_choice])
+
+    computer_choice = random.randint(0, 2)
+    print("Computer chose:")
+    print(game_images[computer_choice])
+
+
+    if player_choice == 0 and computer_choice == 2:
+        print("You win!")
+    elif computer_choice == 0 and player_choice == 2:
+        print("You lose")
+    elif computer_choice > player_choice:
+        print("You lose")
+    elif player_choice > computer_choice:
+        print("You win!")
+    elif computer_choice == player_choice:
+        print("It's a draw")
