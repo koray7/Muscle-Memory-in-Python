b334f2d4c98bd7c86aae7b49c01cc6fe2f4d6cb3
Koray7 <softwarekoray@gmail.com>
2024-07-14 14:29:13 -0500

    Practice of small programs

--- main.py
+++ main.py
@@ -793,75 +793,176 @@ import os
 # game()
 # ***********************-HigherLowerGame-*****************************
 
-from higher_lower_data import data
-import random
+# from higher_lower_data import data
+# import random
 
 
-logo = """
-    __  ___       __             
-   / / / (_)___ _/ /_  ___  _____
-  / /_/ / / __ `/ __ \/ _ \/ ___/
- / __  / / /_/ / / / /  __/ /    
-/_/ ///_/\__, /_/ /_/\___/_/     
-   / /  /____/_      _____  _____
-  / /   / __ \ | /| / / _ \/ ___/
- / /___/ /_/ / |/ |/ /  __/ /    
-/_____/\____/|__/|__/\___/_/     
-"""
+# logo = """
+#     __  ___       __             
+#    / / / (_)___ _/ /_  ___  _____
+#   / /_/ / / __ `/ __ \/ _ \/ ___/
+#  / __  / / /_/ / / / /  __/ /    
+# /_/ ///_/\__, /_/ /_/\___/_/     
+#    / /  /____/_      _____  _____
+#   / /   / __ \ | /| / / _ \/ ___/
+#  / /___/ /_/ / |/ |/ /  __/ /    
+# /_____/\____/|__/|__/\___/_/     
+# """
 
-vs = """
- _    __    
-| |  / /____
-| | / / ___/
-| |/ (__  ) 
-|___/____(_)
-"""
+# vs = """
+#  _    __    
+# | |  / /____
+# | | / / ___/
+# | |/ (__  ) 
+# |___/____(_)
+# """
 
-def format_data(account):
-    """Takes the account data and returns the printable format"""
-    account_name = account["name"]
-    account_descr = account["description"]
-    account_country = account["country"]
-    return (f"{account_name}, a {account_descr}, from {account_country}")
+# def format_data(account):
+#     """Takes the account data and returns the printable format"""
+#     account_name = account["name"]
+#     account_descr = account["description"]
+#     account_country = account["country"]
+#     return (f"{account_name}, a {account_descr}, from {account_country}")
 
-def check_answer(guess, a_followers, b_followers):
-    """Taking the user guess and follower counts and returns if they got it right"""
-    if a_followers > b_followers:
-        return guess == "a"
+# def check_answer(guess, a_followers, b_followers):
+#     """Taking the user guess and follower counts and returns if they got it right"""
+#     if a_followers > b_followers:
+#         return guess == "a"
+#     else:
+#         return guess == "b"
+
+# print(logo)
+# score = 0
+# game_should_continue = True
+# account_b = random.choice(data)
+
+
+# while game_should_continue:
+#     account_a = account_b
+#     account_b = random.choice(data)
+
+#     while account_a == account_b:
+#         account_b = random.choice(data)
+
+
+#     print(f"Compare A: {format_data(account_a)}.")
+#     print(vs)
+#     print(f"Against B: {format_data(account_b)}.")
+
+#     guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+#     a_follower_count = account_a["follower_count"]
+#     b_follower_count = account_b["follower_count"]
+
+#     is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+#     os.system('clear')
+#     print(logo)
+
+#     if is_correct:
+#         score += 1
+#         print(f"\nYou're right! Current score: {score}.")
+#     else:
+#         game_should_continue = False
+#         print(f"Sorry, that's wrong. Final score: {score}.")
+
+# ****************************-Coffee_Machine***************************
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+profit = 0
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+
+def is_resource_sufficient(order_ingredients):
+    """Returns True when order can be made, False if ingredients are insufficient."""
+    for item in order_ingredients:
+        if order_ingredients[item] > resources[item]:
+            print(f"​Sorry there is not enough {item}.")
+            return False
+    return True
+
+
+def process_coins():
+    """Returns the total calculated from coins inserted."""
+    print("Please insert coins.")
+    total = int(input("how many quarters?: ")) * 0.25
+    total += int(input("how many dimes?: ")) * 0.1
+    total += int(input("how many nickles?: ")) * 0.05
+    total += int(input("how many pennies?: ")) * 0.01
+    return total
+
+
+def is_transaction_successful(money_received, drink_cost):
+    """Return True when the payment is accepted, or False if money is insufficient."""
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is ${change} in change.")
+        global profit
+        profit += drink_cost
+        return True
     else:
-        return guess == "b"
-
-print(logo)
-score = 0
-game_should_continue = True
-account_b = random.choice(data)
+        print("Sorry that's not enough money. Money refunded.")
+        return False
 
 
-while game_should_continue:
-    account_a = account_b
-    account_b = random.choice(data)
+def make_coffee(drink_name, order_ingredients):
+    """Deduct the required ingredients from the resources."""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name} ☕️. Enjoy!")
 
-    while account_a == account_b:
-        account_b = random.choice(data)
 
+is_on = True
 
-    print(f"Compare A: {format_data(account_a)}.")
-    print(vs)
-    print(f"Against B: {format_data(account_b)}.")
-
-    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
-
-    a_follower_count = account_a["follower_count"]
-    b_follower_count = account_b["follower_count"]
-
-    is_correct = check_answer(guess, a_follower_count, b_follower_count)
-
-    os.system('clear')
-    print(logo)
-
-    if is_correct:
-        score += 1
-        print(f"\nYou're right! Current score: {score}.")
+while is_on:
+    choice = input("What would you like? (espresso/latte/cappuccino): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${profit}")
     else:
-        game_should_continue = False
-        print(f"Sorry, that's wrong. Final score: {score}.")
+        drink = MENU[choice]
+        if is_resource_sufficient(drink["ingredients"]):
+            payment = process_coins()
+            if is_transaction_successful(payment, drink["cost"]):
+                make_coffee(choice, drink["ingredients"])
+
+
+
+
+
+
+
+
